cb4e5f9fda3949b135540976daf63c1807087bf6
Araon39 <115640752+Araon39@users.noreply.github.com>
2025-01-18 16:30:06 +0200

    Add UI score
    Добавил UI очков

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Добавьте это для использования TextMeshPro компонентов
 
-// Класс Score наследуется от MonoBehaviour и используется для подсчета очков
 public class Score : MonoBehaviour
 {
     // Переменная для хранения количества собранных колец
     private int ring = 0;
+
+    // Ссылка на компонент TextMeshProUGUI для отображения очков
+    public TextMeshProUGUI scoreText;
 
     // Метод OnTriggerExit вызывается, когда объект покидает триггерную область
     private void OnTriggerExit(Collider other)
@@ -20,8 +23,19 @@ public class Score : MonoBehaviour
             // Уничтожаем объект кольца
             Destroy(other.gameObject);
 
-            // Выводим количество собранных колец в консоль для отладки
-            Debug.Log(ring);
+            // Обновляем текст на UI
+            UpdateScoreText();
+        }
+    }
+
+    // Метод для обновления текста очков на UI
+    private void UpdateScoreText()
+    {
+        // Проверяем, что ссылка на компонент TextMeshProUGUI не пустая
+        if (scoreText != null)
+        {
+            // Обновляем текст на UI
+            scoreText.text = "Rings: " + ring.ToString();
         }
     }
 }
